c0e1c6b2a80fb74d7f7f18e006a1dc40ba79e007
venomVV <venomx2020x@gmail.com>
2022-04-22 11:45:52 +0200

    new commit

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
 const categoriesData = require("./data/categoriesData");
 const popularData = require("./data/popularData");
-const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
